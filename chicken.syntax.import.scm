86c8908a0609b1cd368bc937f24c1891016250a1
felix <felix@call-with-current-continuation.org>
2023-07-14 13:16:25 +0200

    rename remaining explicit reference to read/source-info in hardcoded chicken-syntax import library

--- chicken.syntax.import.scm
+++ chicken.syntax.import.scm
@@ -32,7 +32,7 @@
  'expand
  '((expand . chicken.syntax#expand)
    (get-line-number . chicken.syntax#get-line-number)
-   (read/source-info . chicken.syntax#read/source-info)
+   (read-with-source-info . chicken.syntax#read-with-source-info)
    (strip-syntax . chicken.syntax#strip-syntax)
    (syntax-error . chicken.syntax#syntax-error)
    (er-macro-transformer . chicken.syntax#er-macro-transformer)
