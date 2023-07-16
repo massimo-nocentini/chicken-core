10a1ed4ff8611ebb3d74ae433155af6feafdd1a1
felix <felix@call-with-current-continuation.org>
2023-07-16 12:22:07 +0200

    accept dashes in file-version information transmitted via http in chicken-install
    Signed-off-by: Mario Domenech Goulart <mario@parenteses.org>

--- egg-download.scm
+++ egg-download.scm
@@ -149,7 +149,7 @@
         (cond ((or (eof-object? ln)
    	           (irregex-match " *#!eof *" ln))
 	        (open-input-string ""))
-	       ((irregex-match " *#\\|[- ]*([^- ]*) *\\|#.*" ln) =>
+	       ((irregex-match " *#\\|-+ +([^| ]*) *\\|#.*" ln) =>
 	         (lambda (m)
 		   (let ((v (irregex-match-substring m 1)))
 		     (cond ((or (string=? "" v) (string=? "#f" v)))
