massimo-nocentini/chicken-core

commit 6f696411db4deeb6be6eca9e61e85c539bb75c68

bootstrap.sh: Use ftp instead of wget on the BSDs
NetBSD, MirBSD, OpenBSD and DragonFly BSD's base systems ship
ftp(1) (which supports HTTP[S]), but not wget.

On FreeBSD, the version of ftp(1) seems to lag behind the
upstream NetBSD version, and doesn't support HTTPS (only HTTP).
So there, we keep using fetch(1).

Signed-off-by: felix <felix@call-with-current-continuation.org>

scripts/bootstrap.sh
@@ -9,8 +9,12 @@ mkcmd=make
 case "$(uname)" in
     FreeBSD)
         mkcmd=gmake
+	# FreeBSD's ftp doesn't support HTTPS (only HTTP)
         getcmd=fetch;;
     *BSD)
+	# Counter-intuitively, the ftp(1) program on many
+	# BSDs supports both HTTP(S) and FTP
+        getcmd=ftp;;
         mkcmd=gmake;;
 esac
 

commit 5126233351889ed797e4b23e604b5f93058edce3

bootstrap.sh: Fix case syntax error
Fix by Pietro Cerutti submitted to chicken-hackers:
https://lists.nongnu.org/archive/html/chicken-hackers/2024-12/msg00024.html

The issue was introduced in 6f696411db4deeb6be6eca9e61e85c539bb75c68

scripts/bootstrap.sh
@@ -14,7 +14,7 @@ case "$(uname)" in
     *BSD)
 	# Counter-intuitively, the ftp(1) program on many
 	# BSDs supports both HTTP(S) and FTP
-        getcmd=ftp;;
+        getcmd=ftp
         mkcmd=gmake;;
 esac
 