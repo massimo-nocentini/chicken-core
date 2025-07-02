ac0741ccadeb3187a699d08d6bbfe611c43c11ae
felix <felix@call-with-current-continuation.org>
2025-07-02 15:40:57 +0100

    avoid single quotes

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -251,10 +251,10 @@ $compile syntax-tests-2.scm
 ./a.out
 
 echo "======================================== meta-syntax tests ..."
-$interpret -bnq meta-syntax-test.scm -e '(import foo)' -e "(assert (equal? '((1)) (bar 1 2)))" -e "(assert (equal? '(list 1 2 3) (listify)))" -e "(import test-import-syntax-for-syntax)" -e "(assert (equal? '(1) (test)))" -e "(import test-begin-for-syntax)" -e "(assert (equal? '(1) (test)))"
+$interpret -bnq meta-syntax-test.scm -e '(import foo)' -e '(assert (equal? (quote ((1))) (bar 1 2)))' -e '(assert (equal? (quote (list 1 2 3)) (listify)))' -e '(import test-import-syntax-for-syntax)' -e '(assert (equal? (quote (1)) (test)))' -e '(import test-begin-for-syntax)' -e '(assert (equal? (quote (1)) (test)))'
 $compile_s meta-syntax-test.scm -j foo
 $compile_s foo.import.scm
-$interpret -bnq meta-syntax-test.scm -e '(import foo)' -e "(assert (equal? '((1)) (bar 1 2)))" -e "(assert (equal? '(list 1 2 3) (listify)))" -e "(import test-import-syntax-for-syntax)" -e "(assert (equal? '(1) (test)))" -e "(import test-begin-for-syntax)" -e "(assert (equal? '(1) (test)))"
+$interpret -bnq meta-syntax-test.scm -e '(import foo)' -e '(assert (equal? (quote ((1))) (bar 1 2)))' -e '(assert (equal? (quote (list 1 2 3)) (listify)))' -e '(import test-import-syntax-for-syntax)' -e '(assert (equal? (quote (1)) (test)))' -e '(import test-begin-for-syntax)' -e '(assert (equal? (quote (1)) (test)))'
 
 echo "======================================== reexport tests ..."
 $interpret -bnq reexport-tests.scm
