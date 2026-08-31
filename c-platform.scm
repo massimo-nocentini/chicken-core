a46207a6db1b949a0c252c0619c8f6c37ef0cb38
felix <felix@call-with-current-continuation.org>
2026-08-31 23:00:38 +0200

    add -merge-* options to list of options in c-platform.scm to avoid warning (reported by kon)

--- c-platform.scm
+++ c-platform.scm
@@ -106,7 +106,7 @@
     no-procedure-checks-for-toplevel-bindings
     no-bound-checks no-procedure-checks-for-usual-bindings no-compiler-syntax
     no-parentheses-synonyms r7rs-syntax emit-all-import-libraries
-    strict-types lfa2 debug-info
+    strict-types lfa2 debug-info merge-reusable-closures merge-shareable-closures
     regenerate-import-libraries setup-mode
     module-registration no-module-registration))
 
