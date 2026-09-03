3c983fadc7abe9bc3d82f58839950ef1b272d31e
felix <felix@call-with-current-continuation.org>
2026-09-03 22:40:46 +0200

    banner arrangements

--- banner.scm
+++ banner.scm
@@ -2,10 +2,10 @@
 
 
 (define-constant +banner+ #<<EOF
-   ____,
-  /   ^ >   CHICKEN
- /| ____    (c)2000-2007 Felix L. Winkelmann, (c)2008 The CHICKEN Team
- ||/    \\  XXX
+   ____,     CHICKEN
+  /   ^ >    (c) 2008 The CHICKEN Team
+ /| ____     (c) 2000-2007 Felix L. Winkelmann
+ ||/    \\   XXX
  ||     ||
   \\____/YYY
 EOF
